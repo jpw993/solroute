11f43e2ae46a74c0f0ddfc288aa878fae1da4502
James Walker <jpw993@gmail.com>
2025-06-15 08:30:13 +0000

    the icons are correct, but are not displaying properly

--- src/components/icons/tokens/EthereumIcon.tsx
+++ src/components/icons/tokens/EthereumIcon.tsx
@@ -4,10 +4,10 @@ export function EthereumIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path d="M15.996 2L15.562 3.002L9.048 16.357L15.996 20.016L22.944 16.357L16.429 3.002L15.996 2Z" fill="#8A9EF9"/>
-      <path d="M15.996 21.206L-6.948 16.356L15.996 29.87L22.944 16.357L15.996 21.206Z" fill="#627EEA"/>
-      <path d="M15.996 20.016L22.944 16.357L15.996 2V18.016Z" fill="#4560D1"/> {/* Adjusted V coordinate */}
+      <path d="M15.996 20.016L9.048 16.357L15.996 29.87L22.944 16.357L15.996 20.016Z" fill="#627EEA"/>
+      <path d="M15.996 20.016L22.944 16.357L15.996 2V18.016Z" fill="#4560D1"/>
       <path d="M9.048 16.357L15.996 20.016V2L9.048 16.357Z" fill="#8A9EF9"/>
-      <path d="M15.996 21.206L22.944 16.357L15.996 18.022V21.206Z" fill="#4560D1"/> {/* Adjusted V coordinate */}
+      <path d="M15.996 21.206L22.944 16.357L15.996 18.022V21.206Z" fill="#4560D1"/>
       <path d="M9.048 16.357L15.996 21.206V18.022L9.048 16.357Z" fill="#627EEA"/>
     </svg>
   );
